d02f51c5f2fd701ed14320ffb02fca7423002ded
Lukas B. Nordentoft <Lukasbnordentoft@gmail.com>
2023-05-31 22:37:03 +0200

    upload dist

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pypsa_netview',
-    version='0.0.1',    
+    version='0.1.0',    
     description='Package for visualizing PyPSA Networks',
     url='https://github.com/LukasBNordentoft/pypsa_netview',
     author='Lukas B. Nordentoft',
